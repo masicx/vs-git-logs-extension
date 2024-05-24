a4de63417e56cf8c00b41c45de33e1c85ac630d0
Marthell Iniguez <masicx@gmail.com>
2024-05-24 17:29:56 -0400

    Remove python code.

--- src/python/import.py
+++
@@ -1,3 +0,0 @@
-import os
-import subprocess;
-os.system("py ./src/python/git-logs.py '2020-06-25' -d c:/Users/salo3/Documents/git-logs-main C:/Users/salo3/Code/VsCodeExtensions/git-logs-report -v")
